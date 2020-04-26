1bce84dc758e268b4aae2a414cdc8c781cf9a3a2
Krishna Prasath Soundararajan <krishna2793@gmail.com>
2020-04-26 01:15:48 -0700

    add pytest

--- bachelor_kitchen/routes.py
+++ bachelor_kitchen/routes.py
@@ -1,8 +1,8 @@
-from bachelor_kitchen import app, db, bcrypt
+from bachelor_kitchen import app
+from bachelor_kitchen.persistance_helper import persistUser, getUser
 from flask import render_template, url_for, flash, redirect, request, session, send_from_directory, make_response
 from flask_login import login_user, current_user, logout_user, login_required
 from bachelor_kitchen.forms import RegistrationForm, LoginForm
-from bachelor_kitchen.models import User
 
 
 @app.route("/", methods=['GET'])
@@ -18,21 +18,19 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         # hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data,
-                    email=form.email.data,
-                    password=form.password.data,
-                    firstname=form.firstname.data,
-                    lastname=form.lastname.data,
-                    phonenum=form.phonenum.data,
-                    address=form.address.data,
-                    city=form.city.data,
-                    zipcode=form.zipcode.data,
-                    state=form.state.data,
-                    dob=form.dob.data,
-                    university=form.university.data
-                    )
-        db.session.add(user)
-        db.session.commit()
+        persistUser(username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+            firstname=form.firstname.data,
+            lastname=form.lastname.data,
+            phonenum=form.phonenum.data,
+            address=form.address.data,
+            city=form.city.data,
+            zipcode=form.zipcode.data,
+            state=form.state.data,
+            dob=form.dob.data,
+            university=form.university.data
+            )
         flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('register'))
     return render_template('register.html', title='Register', form=form)
@@ -44,8 +42,8 @@ def login():
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and user.password == form.password.data:
+        user = getUser(form.email.data, form.password.data)
+        if user:
             login_user(user, remember=False)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home'))
--- tests/test_flaskr.py
+++ tests/test_flaskr.py
@@ -2,19 +2,23 @@ import os
 import tempfile
 
 import pytest
+import sys
+sys.path.append('../bachelor_kitchen')
+from bachelor_kitchen import app
 
-from flaskr import flaskr
 
-
-@pytest.fixture
-def client():
-    db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-    flaskr.app.config['TESTING'] = True
-
-    with flaskr.app.test_client() as client:
-        with flaskr.app.app_context():
-            flaskr.init_db()
-        yield client
-
-    os.close(db_fd)
-    os.unlink(flaskr.app.config['DATABASE'])
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = app
+ 
+    # Flask provides a way to test your application by exposing the Werkzeug test Client
+    # and handling the context locals for you.
+    testing_client = flask_app.test_client()
+ 
+    # Establish an application context before running the tests.
+    ctx = flask_app.app_context()
+    ctx.push()
+ 
+    yield testing_client  # this is where the testing happens!
+ 
+    ctx.pop()

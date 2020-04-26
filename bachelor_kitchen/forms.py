c0c05adc4cefc795d40a787c2dc0eb05143be2b6
Harsha Vardhan <harshavardhan@Harshas-MacBook-Air.local>
2020-04-25 20:56:10 -0700

    login and db

--- bachelor_kitchen/forms.py
+++ bachelor_kitchen/forms.py
@@ -1,60 +1,79 @@
-from bachelor_kitchen.models import User,Post
+from bachelor_kitchen.models import User, Post
 from flask_login import current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField,IntegerField,DateField,SelectField,RadioField,DecimalField,TextAreaField,FloatField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError,Regexp, InputRequired
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, DateField, SelectField, RadioField, DecimalField, TextAreaField, FloatField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp, InputRequired
 from datetime import datetime, date
 
+
 def phone_length_check(form, field):
-    if (len(str(field.data))!= 10):
+    if (len(str(field.data)) != 10):
         raise ValidationError('Phone number must be 10 digits')
 
 
-
 class RegistrationForm(FlaskForm):
-    username = StringField('User Name', 
-            validators=[DataRequired(), 
-                        Length(min=2, max=20),
-                        Regexp('^\w+$',message="Only alphanumeric and underscore allowed")])
-    firstname = StringField('Firstname', 
-            validators=[DataRequired(),
-                        Length(min=2, max=20), 
-                        Regexp('^[.0-9a-zA-Z\s-]+$', message="Not a valid name")])
-    lastname = StringField('LastName', 
-            validators=[DataRequired(), 
-                        Length(min=2, max=20), 
-                        Regexp('^[.0-9a-zA-Z\s-]+$', message="Not a valid name")])
+    username = StringField('User Name',
+                           validators=[DataRequired(),
+                                       Length(min=2, max=20),
+                                       Regexp('^\w+$', message="Only alphanumeric and underscore allowed")])
+    firstname = StringField('Firstname',
+                            validators=[DataRequired(),
+                                        Length(min=2, max=20),
+                                        Regexp('^[.0-9a-zA-Z\s-]+$', message="Not a valid name")])
+    lastname = StringField('LastName',
+                           validators=[DataRequired(),
+                                       Length(min=2, max=20),
+                                       Regexp('^[.0-9a-zA-Z\s-]+$', message="Not a valid name")])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', 
-            validators=[DataRequired(),
-                        Regexp('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$', message="Must have atleast 1 uppercase, 1 lowercase, 1 digit and of size 6")])
-    confirm_password = PasswordField('Confirm Password', 
-            validators=[DataRequired(), 
-                        EqualTo('password')])
-    phonenum=StringField('Phone Number', render_kw={"placeholder": "XXX-XXX-XXXX"}, 
-            validators=[DataRequired(),
-                        Regexp('^\d{3}-\d{3}-\d{4}$', message='Phone number is not valid. Enter in xxx-xxx-xxxx format')])
-    address=TextAreaField('Address', validators=[Regexp('^[#.0-9a-zA-Z\s,-]+$', message="Not a valid address")])
-    city=TextAreaField('City', validators=[Regexp('^[#.0-9a-zA-Z\s,-]+$', message="Not a valid city")])
-    zipcode=TextAreaField('Zip Code', validators=[Regexp('^[#.0-9a-zA-Z\s,-]+$', message="Not a valid zipcode")])
-    state=TextAreaField('state', validators=[Regexp('^[#.0-9a-zA-Z\s,-]+$', message="Not a valid state")])
-    dob=DateField('Date of Birth',format='%m/%d/%Y',validators=[DataRequired()],render_kw={"placeholder": "MM/DD/YYYY"})
-    university=TextAreaField('University', validators=[Regexp('^[#.0-9a-zA-Z\s,-]+$', message="Not a valid university")])
+    password = PasswordField('Password',
+                             validators=[DataRequired(),
+                                         Regexp('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$', message="Must have atleast 1 uppercase, 1 lowercase, 1 digit and of size 6")])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
+    phonenum = StringField('Phone Number', render_kw={"placeholder": "XXX-XXX-XXXX"},
+                           validators=[DataRequired(),
+                                       Regexp('^\d{3}-\d{3}-\d{4}$', message='Phone number is not valid. Enter in xxx-xxx-xxxx format')])
+    address = TextAreaField('Address', validators=[Regexp(
+        '^[#.0-9a-zA-Z\s,-]+$', message="Not a valid address")])
+    city = TextAreaField('City', validators=[Regexp(
+        '^[#.0-9a-zA-Z\s,-]+$', message="Not a valid city")])
+    zipcode = TextAreaField('Zip Code', validators=[Regexp(
+        '^[#.0-9a-zA-Z\s,-]+$', message="Not a valid zipcode")])
+    state = TextAreaField('state', validators=[Regexp(
+        '^[#.0-9a-zA-Z\s,-]+$', message="Not a valid state")])
+    dob = DateField('Date of Birth', format='%m/%d/%Y',
+                    validators=[DataRequired()], render_kw={"placeholder": "MM/DD/YYYY"})
+    university = TextAreaField('University', validators=[Regexp(
+        '^[#.0-9a-zA-Z\s,-]+$', message="Not a valid university")])
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That userid is taken. Please choose a different one.')
+            raise ValidationError(
+                'That userid is taken. Please choose a different one.')
+
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
-    def validate_dob(self,dob):
-        dob_val=dob.data
-        if(dob_val>=date.today()):
+            raise ValidationError(
+                'That email is taken. Please choose a different one.')
+
+    def validate_dob(self, dob):
+        dob_val = dob.data
+        if(dob_val >= date.today()):
             raise ValidationError('Date of Birth is not valid!')
-    def validate_phonenum(self,phonenum):
-        phone=User.query.filter_by(phonenum=phonenum.data).first()
+
+    def validate_phonenum(self, phonenum):
+        phone = User.query.filter_by(phonenum=phonenum.data).first()
         if phone:
-            raise ValidationError('That phone number is already registered with us.')
+            raise ValidationError(
+                'That phone number is already registered with us.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
--- bachelor_kitchen/routes.py
+++ bachelor_kitchen/routes.py
@@ -1,17 +1,15 @@
-from bachelor_kitchen import app,db
-from flask import render_template, url_for, flash, redirect, request,session, send_from_directory, make_response
+from bachelor_kitchen import app, db, bcrypt
+from flask import render_template, url_for, flash, redirect, request, session, send_from_directory, make_response
 from flask_login import login_user, current_user, logout_user, login_required
-from bachelor_kitchen.forms import RegistrationForm
+from bachelor_kitchen.forms import RegistrationForm, LoginForm
 from bachelor_kitchen.models import User
 
 
-
-
-
 @app.route("/", methods=['GET'])
+@app.route("/home", methods=['GET'])
 def home():
-    
     return render_template('layout.html')
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -20,21 +18,43 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         # hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, 
-            email=form.email.data,
-            password=form.password.data,
-            firstname=form.firstname.data,
-            lastname=form.lastname.data,
-            phonenum=form.phonenum.data,
-            address=form.address.data,
-            city = form.city.data,
-            zipcode = form.zipcode.data,
-            state = form.state.data,
-            dob=form.dob.data,
-            university = form.university.data
-            )
+        user = User(username=form.username.data,
+                    email=form.email.data,
+                    password=form.password.data,
+                    firstname=form.firstname.data,
+                    lastname=form.lastname.data,
+                    phonenum=form.phonenum.data,
+                    address=form.address.data,
+                    city=form.city.data,
+                    zipcode=form.zipcode.data,
+                    state=form.state.data,
+                    dob=form.dob.data,
+                    university=form.university.data
+                    )
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('register'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.password == form.password.data:
+            login_user(user, remember=False)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))

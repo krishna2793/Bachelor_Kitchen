d0f3481d2c406b8898d9be895f1e51c4833dff0f
Krishna Prasath Soundararajan <krishna2793@gmail.com>
2021-01-06 11:15:21 -0500

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,4 @@ import os
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    secret = str(os.environ.get('SECRET_KEY', ''))
-    app.run(host='0.0.0.0', port=port, debug = True, secret=secret)
+    app.run(host='0.0.0.0', port=port, debug = True)

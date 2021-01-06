300aee5d2c1ef3868eb06ee7cc10c291fa673d35
Krishna Prasath Soundararajan <krishna2793@gmail.com>
2021-01-06 11:16:32 -0500

    Update __init__.py
    Add Secret from environment

--- bachelor_kitchen/__init__.py
+++ bachelor_kitchen/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = ''
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SECRET_KEY'] = str(os.environ.get('SECRET_KEY', ''))
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.environ.get('SQLALCHEMY_DATABASE_URI', ''))
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)

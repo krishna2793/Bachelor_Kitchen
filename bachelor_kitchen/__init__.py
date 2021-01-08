f11724f0e9d953921bc80dcca63da65308ae4cdd
Krishna Prasath Soundararajan <krishna2793@gmail.com>
2021-01-08 06:43:51 -0500

    Mail server configure

--- bachelor_kitchen/__init__.py
+++ bachelor_kitchen/__init__.py
@@ -22,8 +22,8 @@ mail_settings = {
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": os.environ['MAIL_USERNAME'],
-    "MAIL_PASSWORD": os.environ['MAIL_PASSWORD']
+    "MAIL_USERNAME": os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
 }
 app.config.update(mail_settings)
 mail = Mail(app)

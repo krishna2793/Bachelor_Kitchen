10a71c5ebdb36423cd4f9beb8f65efcf000ef9ff
Krishna Prasath Soundararajan <krishna2793@gmail.com>
2021-01-14 05:01:57 -0500

    Fix error in direction API

--- bachelor_kitchen/routes.py
+++ bachelor_kitchen/routes.py
@@ -258,9 +258,8 @@ def view_registered_users(post_id):
 def directions(post_id):
     post = Post.query.get_or_404(post_id)
     user = User.query.filter_by(username=post.author.username).first()
-    source = current_user.address + current_user.city + \
-        current_user.state + current_user.zipcode
-    destination = user.address + user.city + user.state + user.zipcode
+    source = " ".join([current_user.address, current_user.city, current_user.state, current_user.zipcode])
+    destination = " ".join([user.address, user.city, user.state, user.zipcode])
     return render_template('directions.html', source=source, destination=destination)
 
 
